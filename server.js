5c8648cb324a888726fbb0966cc7d88814a359ea
NishanthG69 <105039147+NishanthG69@users.noreply.github.com>
2026-01-02 08:27:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -291,8 +291,11 @@ app.post("/api/order/status", (req, res) => {
 });
 
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log("Server running on port", PORT);
 });
+
+
 
 
